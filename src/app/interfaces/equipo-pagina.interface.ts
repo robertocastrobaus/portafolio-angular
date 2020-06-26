ca334448fc8711870eceb49073c5b30b696a034d
Isabel Aviles <isabel_86avh@hotmail.com>
2020-06-26 02:14:21 -0500

    firebase service

---
+++ src/app/interfaces/equipo-pagina.interface.ts
@@ -0,0 +1,7 @@
+export interface equipoPagina{
+    frase?: string;
+    nombre?: string;
+    subtitulo?:string;
+    tiwtter?:string;
+    url?: string;
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ComponentFactoryResolver } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { infoPagina } from '../interfaces/info-pagina.interface';
+import { equipoPagina } from '../interfaces/equipo-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,27 @@ import { infoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: infoPagina ={}; // arreglo
   cargada = false;
+  equipo: equipoPagina[]=[];
+  carEquipo =false;
   constructor(private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: infoPagina) =>{      
       this.cargada=true;
       this.info=resp;
       console.log(this.info);
 
+    });
+  }
+  private cargarEquipo(){
+    this.http.get('https://angula-html-a5e02.firebaseio.com/equipo.json').subscribe( (resp :equipoPagina[]) =>{
+      this.carEquipo=true;
+      this.equipo = resp;
+      console.log(this.equipo);
     });
   }
 }
