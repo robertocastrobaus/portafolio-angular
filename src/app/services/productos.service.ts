a369af66235989b5d932b15e6984f577f88a5b11
Isabel Aviles <isabel_86avh@hotmail.com>
2020-07-08 03:39:30 -0500

    listo prod

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
